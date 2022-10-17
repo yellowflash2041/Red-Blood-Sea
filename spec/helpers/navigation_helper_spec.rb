f42ec2692d64de09554746211b9463e621481bf1
yellowflash2041 <yellowflash2041@gmail.com>
2022-10-17 15:52:58 +0900

    add sign up features specs

--- spec/helpers/navigation_helper_spec.rb
+++ spec/helpers/navigation_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe NavigationHelper, :type => :helper do
 
   context 'signed in user' do
-    before(:each) { helper.stub(:user_signed_in?).and_return(true) }
+    before(:each) { allow(helper).to receive(:user_signed_in?).and_return(true) }
 
     context '#collapsible_links_partial_path' do
       it "returns signed_in_links partial's path" do
@@ -15,7 +15,7 @@ RSpec.describe NavigationHelper, :type => :helper do
   end
 
   context 'non-signed in user' do
-    before(:each) { helper.stub(:user_signed_in?).and_return(false) }
+    before(:each) { allow(helper).to receive(:user_signed_in?).and_return(false) }
 
     context '#collapsible_links_partial_path' do
       it "returns non_signed_in_links partial's path" do
