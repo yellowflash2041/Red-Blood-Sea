9c344b23019416f899c08a87d74b2d9bd39b8e47
yellowflash2041 <yellowflash2041@gmail.com>
2022-10-22 04:58:08 +0900

    Render a private conversation window on the app - Add opened conversations to the session - Create a _conversation.html.erb file inside private/conversations - Define a private_conv_recipient helper method in the private/conversations_helper.rb - Define an opposed_user method in Private::Conversation model and add specs for it - Create _heading.html.erb and _messages_list.html.erb files inside the private/conversations/conversation - Define a load_private_messages in private/conversations_helper.rb and add specs for it - Create a _new_message.html.erb inside the private/conversations/conversation - Create a _open.js.erb inside private/conversations - Create a position_and_visibility.js inside the /javascript/packs/conversations - Create a conversation_window.scss inside the assets/stylesheets/partials - Define an opened_conversations_windows helper method in ApplicationController - Create a _private_conversations_windows.html.erb inside the layouts/application

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem 'faker'
 
 gem 'will_paginate', '~> 3.1.0'
 
+gem 'gon'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -1,9 +1,9 @@
 class Private::ConversationsController < ApplicationController
   def create
     recipient_id = Post.find(params[:post_id]).user.id
-    conversation = Private::Conversation.new(sender_id: current_user.id, recipient_id: recipient_id)
-    if conversation.save
-      Private::Message.create(user_id: recipient_id, conversation_id: conversation.id, body: params[:message_body])
+    @conversation = Private::Conversation.new(sender_id: current_user.id, recipient_id: recipient_id)
+    if @conversation.save
+      Private::Message.create(user_id: recipient_id, conversation_id: @conversation.id, body: params[:message_body])
       respond_to do |format|
         format.js {render partial: 'posts/show/contact_user/message_form/success'}
       end
@@ -12,5 +12,16 @@ class Private::ConversationsController < ApplicationController
         format.js {render partial: 'posts/show/contact_user/message_form/fail'}
       end
     end
+  end
+
+  private
+
+  def add_to_conversations
+    session[:private_conversations] ||= []
+    session[:private_conversations] << @conversation.id
+  end
+
+  def already_added?
+    session[:private_conversations].include?(@conversation.id)
   end
 end
