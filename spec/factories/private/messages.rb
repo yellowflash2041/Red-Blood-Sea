e7128aabdb6a3af93bf5d0b307c3064de95fbb80
yellowflash2041 <yellowflash2041@gmail.com>
2022-10-21 16:47:37 +0900

    Inside a post add a form to contact a user - Define a contact_user_partial_path helper method in PostsHelper. Add specs for the method - Create _contact_user.html.erb and _login_required.html.erb partials - Define a leave_message_partial_path helper method in PostsHelper. Add specs for the method - Create _already_in_touch.html.erb and _message_form.html.erb partial files - Define a @message_has_been_sent in PostsController's show action - Define a between_users scope inside the Private::Conversation model Add specs for the scope - Define private_conversation and private_message factories - Define routes for Private::Conversations and Private::Messages - Define a create action inside the Private::Conversations - Create _success.js and _fail.js partials - Add feature specs to test the overall .contact-user form

--- spec/factories/private/messages.rb
+++ spec/factories/private/messages.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :private_message, class: 'Private::Message' do
-    
+    body { 'aaaaaaaaaaaaaaaaaaaa' }
+    association :conversation, factory: :private_conversation
+    user
   end
 end
